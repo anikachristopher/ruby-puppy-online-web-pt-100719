bbc18b15ea3e806bb719d4f052a137c715bbf22a
Anika Christopher <anika.mchristopher@gmail.com>
2019-11-05 16:46:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
-# Add your code here
+class Dog 
+  
+  @@all = []
+  
+  attr_accessor :name 
+  
+  def initialize(dog)
+    @dog = dog
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end  
+  
+  def self.print_all
+    
+end  
